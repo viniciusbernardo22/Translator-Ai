5421021e4e300b8ca3f9bb5d19f36f3a6e178a0d
Vinícius <vini383@gmail.com>
2025-09-27 12:39:17 -0300

    rm cors

--- Translator-Ai/Program.cs
+++ Translator-Ai/Program.cs
@@ -2,24 +2,13 @@ using Translator_Ai.Infraestructure.Configurations.Translator;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration
+       .AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAzureTranslator(builder.Configuration);
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAngularDev", policy =>
-    {
-        policy.WithOrigins(
-                "https://localhost:4200",
-                "https://frontend-translator-ai.vercel.app",
-                "https://translator-ai-white-resonance-7456.fly.dev"
-            )
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
 
 var app = builder.Build();
 var isProd = app.Environment.IsDevelopment();
@@ -30,7 +19,6 @@ if (!isProd)
     app.UseSwaggerUI();
 }
 
-app.UseCors("AllowAngularDev");
 app.UseAuthorization();
 app.MapControllers();
 
