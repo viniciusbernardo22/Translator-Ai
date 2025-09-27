a9ed481a06b18a05af7843143238d475698cb50f
Vinícius <vini383@gmail.com>
2025-09-27 12:42:21 -0300

    update structure

--- Translator-Ai/Program.cs
+++ Translator-Ai/Program.cs
@@ -5,19 +5,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
        .AddEnvironmentVariables();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(
+                "https://localhost:4200",
+                "https://frontend-translator-ai.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAzureTranslator(builder.Configuration);
 
 var app = builder.Build();
-var isProd = app.Environment.IsDevelopment();
+var isDev = app.Environment.IsDevelopment();
 
-if (!isProd)
+if (isDev)
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 app.MapControllers();
